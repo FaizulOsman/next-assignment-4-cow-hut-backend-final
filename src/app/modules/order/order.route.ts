7fe9d9c55fa90f81efaa91597e5bb80e56171a2e
FaizulOsman <faizulosmanrabby@gmail.com>
2023-06-24 20:23:53 +0600

    feat: authentication added in create order

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { OrderController } from "./order.controller";
 import { OrderValidation } from "./order.validation";
 import validateRequest from "../../middlewares/validateRequest";
+import auth from "../../middlewares/auth";
+import { ENUM_USER_ROLE } from "../../../enums/user";
 const router = express.Router();
 
 // Routes
 router.post(
   "/",
   validateRequest(OrderValidation.createOrderZodSchema),
+  auth(ENUM_USER_ROLE.BUYER),
   OrderController.createOrder
 );
 router.get("/", OrderController.getAllOrders);
